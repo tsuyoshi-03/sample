8f62a10603dd7bbe5f798c8ec08a6f3fc515a7c8
tsuyoshi <zynb0316@icloud.com>
2021-03-31 08:58:38 +0000

    新規登録

--- environment/rails_projects/test1/app/controllers/users_controller.rb
+++ environment/rails_projects/test1/app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    @user = User.new(user_params)
     if @user.save
       redirect_to root_path
     else
       render :new
     end
-  end  
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
   
 end
